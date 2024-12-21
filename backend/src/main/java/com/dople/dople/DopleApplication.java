91aad0a1202410bf9acff12cea1f17bcb85be486
Melihntr <melihavci@yahoo.com>
2024-12-21 18:45:12 +0300

    init

---
+++ backend/src/main/java/com/dople/dople/DopleApplication.java
@@ -0,0 +1,13 @@
+package com.dople.dople;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DopleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DopleApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/dople/dople/DopleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dople.dople;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DopleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
